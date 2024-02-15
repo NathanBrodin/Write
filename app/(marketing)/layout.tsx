f0e2044e3ef15d8d2517c3b9a81c02a4d6a1a5ea
Nathan Brodin <nathan@brodin.dev>
2024-02-15 22:24:49 +0100

    build: implement dark mode

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,7 +5,7 @@ export default function MarketingLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="h-full">
+  return <div className="h-full dark:bg-[#1F1F1F]">
     <main className="h-full pt-40">
         <Navbar />
         {children}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,15 +13,15 @@ export const metadata: Metadata = {
       {
         media: "(prefers-color-scheme: light)",
         url: "/logo.svg",
-        href: "/logo.svg"
+        href: "/logo.svg",
       },
       {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
-        href: "/logo-dark.svg"
-      }
-    ]
-  }
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -29,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="notion-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
