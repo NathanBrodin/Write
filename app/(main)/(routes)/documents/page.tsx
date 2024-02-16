8af738c4a1237592782c753c9f4f8f6a7bc03656
Nathan Brodin <nathan@brodin.dev>
2024-02-16 20:49:55 +0100

    build: implement note creation

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,23 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 export default function DocumentsPage() {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  function onCreate() {
+    const promise = create({title: "Untitled"});
+
+    toast.promise(promise, {
+      loading: "Creating note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    })
+  }
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -27,7 +41,7 @@ export default function DocumentsPage() {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Notion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
