53dfd1a48c767c988efbb41728a52253dae27717
Nathan Brodin <nathan@brodin.dev>
2024-02-16 20:45:29 +0100

    build: implement document creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,6 +43,7 @@ export default function RootLayout({
             storageKey="notion-theme"
           >
             {children}
+            <Toaster position="bottom-center"/>
           </ThemeProvider>
         </ConvexClientProvider>
       </body>
