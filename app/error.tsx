1c1480f8c3a59e515dbd0bc147106d0a08da644b
Nathan Brodin <nathan@brodin.dev>
2024-02-19 18:26:12 +0100

    build: configure error page

---
+++ app/error.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/error.png"
+        height={300}
+        width={300}
+        alt="Error"
+        className="dark:hidden"
+      />
+      <Image
+        src="/error-dark.png"
+        height={300}
+        width={300}
+        alt="Error"
+        className="dark:block hidden"
+      />
+      <h2 className="text-xl font-medium">Something went wrong</h2>
+      <Button asChild>
+        <Link href="/documents">Go back</Link>
+      </Button>
+    </div>
+  );
+}
