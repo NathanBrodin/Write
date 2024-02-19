2ea36ce01953ccb8cee32f1b5abcf845b9759853
Nathan Brodin <nathan@brodin.dev>
2024-02-19 15:51:24 +0100

    build: finished heading

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,3 +1,35 @@
-export default function DocumentIdPage() {
-  return <div>DocumentId</div>;
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+export default function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]"/>
+      <div className="md-max-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document}/>
+      </div>
+    </div>
+  );
 }
