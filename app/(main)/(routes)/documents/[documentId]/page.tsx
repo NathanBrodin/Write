f23ae21c8bc7644ab71004dd6ed96980e2deb937
Nathan Brodin <nathan@brodin.dev>
2024-02-19 18:20:10 +0100

    fix: fixed editor import

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,60 +1,49 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-import { Toolbar } from "@/components/toolbar";
-import { Cover } from "@/components/cover";
-import { Skeleton } from "@/components/ui/skeleton";
-import Editor from "@/components/edtior";
+import { useTheme } from "next-themes";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
 
-interface DocumentIdPageProps {
-  params: {
-    documentId: Id<"documents">;
-  };
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
 }
 
-export default function DocumentIdPage({ params }: DocumentIdPageProps) {
-  const document = useQuery(api.documents.getById, {
-    documentId: params.documentId,
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return response.url;
+  };
+
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpload,
   });
 
-  const update = useMutation(api.documents.update);
-
-  function onChange(content: string) {
-    update({
-      id: params.documentId,
-      content,
-    });
-  }
-
-  if (document === undefined) {
-    return (
-      <div>
-        <Cover.Skeleton />
-        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
-          <div className="space-y-4 pl-8 pt-4">
-            <Skeleton className="h-14 w-[50%]" />
-            <Skeleton className="h-4 w-[80%]" />
-            <Skeleton className="h-4 w-[40%]" />
-            <Skeleton className="h-4 w-[60%]" />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  if (document === null) {
-    return <div>Not found</div>;
-  }
-
   return (
-    <div className="pb-40">
-      <Cover url={document.coverImage} />
-      <div className="md-max-3xl lg:max-w-4xl mx-auto">
-        <Toolbar initialData={document} />
-        <Editor onChange={onChange} initialContent={document.content} />
-      </div>
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
     </div>
   );
-}
+};
+
+export default Editor;
