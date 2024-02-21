7e10390f8ca3a28a787cd23bc932eaa29d31bfd2
Nathan Brodin <nathan@brodin.dev>
2024-02-21 21:48:37 +0100

    build: implement privacy-policy and terms-and-conditions page

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,16 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Logo from "./logo";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <Button variant="ghost" size="sm">
-          Privacy Policy
+        <Button variant="ghost" size="sm" asChild>
+          <Link href="/privacy-policy">Privacy Policy</Link>
         </Button>
-        <Button variant="ghost" size="sm">
-          Terms & Conditions
+        <Button variant="ghost" size="sm" asChild>
+          <Link href="/terms-and-conditions">Terms & Conditions</Link>
         </Button>
       </div>
     </div>
