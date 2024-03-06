6405cb04652349cf9bb98dfa1ef7c111d3baa39d
Nathan Brodin <nathan@brodin.dev>
2024-03-06 21:02:04 +0100

    feat: improved site SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,55 @@ import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Write",
-  description: "Capture, Collaborate, Create.",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    "Markdown",
+    "Editor",
+    "Notes",
+    "Notion",
+    "Markdown Editor",
+    "Markdown images",
+  ],
+  authors: [
+    {
+      name: "Nathan Brodin",
+      url: "https://brodin.dev",
+    },
+  ],
+  creator: "Nathan Brodin",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@shadcn",
+  },
   icons: {
     icon: [
       {
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Write",
+  url: "https://write.brodin.dev",
+  ogImage: "https://write.brodin.dev/editor-sidebar.png",
+  description:
+    "Capture, Collaborate, Create. Welcome to Write is your all-in-one platform for seamless collaboration and creativity.",
+  links: {
+    github: "https://github.com/NathanBrodin/Write",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
