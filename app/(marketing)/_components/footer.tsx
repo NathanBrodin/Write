af403564ee6d21ab6a26d48c41e2c75d3d68ab6a
Nathan Brodin <nathan@brodin.dev>
2024-03-06 20:32:05 +0100

    feat: added github link

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -6,6 +6,11 @@ export default function Footer() {
     <div className="bg-background z-50 flex w-full items-center p-6 dark:bg-[#1F1F1F]">
       <div className="text-muted-foreground flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
         <Button variant="ghost" size="sm" asChild>
+          <Link href="https://github.com/NathanBrodin/Write">
+            Source code (Github)
+          </Link>
+        </Button>
+        <Button variant="ghost" size="sm" asChild>
           <Link href="/privacy-policy">Privacy Policy</Link>
         </Button>
         <Button variant="ghost" size="sm" asChild>
